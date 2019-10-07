498ad474712d83712d3def3cb81d1ebaa6e1675a
cmaatias <55700545+cmaatias@users.noreply.github.com>
2019-10-07 16:25:54 +0200

    Catalogo

---
+++ catalogo.php
@@ -0,0 +1,13 @@
+<html>
+<h2>Enlaces de LInkedin de todos los Compañeros</h2>
+<a href="https://github.com/serran23/asir2_alvaro">Alvaro Serrano</a></br>
+<a href="https://github.com/jooseluis7">Jose Luis</a></br>
+<a href="https://github.com/delasen99">Alejandro Delasen</a></br>
+<a href="https://www.linkedin.com/in/luis-camus-obregon">Luis</a></br>
+<a href="https://github.com/DanielHernandezCantera">Daniel Hernandez</a></br>
+<a href="https://www.linkedin.com/in/kevin-diaz-tomero/">Kevin Diaz</a></br>
+<a href="https://github.com/Junnyper">Kimberly</a></br>
+<a href="https://www.linkedin.com/in/pablo-argumosa-ruiz/">Pablo Argumosa</a></br>
+<a href="https://www.linkedin.com/in/davidjpblanco/">David Blanco</a></br>
+<a href="https://www.linkedin.com/in/jaimegarciacifuentes/">Jaime Garcia</a></br>
+</html>
