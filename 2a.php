60927f96d252fe55636e1656aba65aff96bee187
cmaatias <55700545+cmaatias@users.noreply.github.com>
2019-10-07 16:45:14 +0200

    Variables

---
+++ 2a.php
@@ -0,0 +1,20 @@
+<?php
+$a=37;
+$b=23;
+	echo '<div>';
+		echo 'Suma de '.$a.' + '.$b.' = ';
+		echo $a+$b;
+	echo '</div>';
+	echo '<div>';
+		echo 'Resta de '.$a.' - '.$b.' = ';
+		echo $a-$b;
+	echo '</div>';
+	echo '<div>';
+		echo 'Multiplicación de '.$a.' x '.$b.' = ';
+		echo $a*$b;
+	echo '</div>';
+	echo '<div>';
+		echo 'División de '.$a.' / '.$b.' = ';
+		echo $a/$b;
+	echo '</div>';
+?>
