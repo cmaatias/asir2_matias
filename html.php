6c10ca6b4b885e4880e314099d9c5feb687003ea
cmaatias <55700545+cmaatias@users.noreply.github.com>
2019-11-18 17:23:00 +0100

    HTML.PHP

---
+++ html.php
@@ -0,0 +1,60 @@
+<html>
+
+<head>
+<meta charset="UTF-8"/>
+<title>PROVINCIAS</title>
+</head>
+
+<body>
+
+<center>
+
+<h1>PROVINCIAS BAÑADAS POR EL CANTABRICO</h1>
+
+<img src="mapa.jpg" alt="MAPA DEL MAR CANTABRICO" width="500" height="400"/>
+
+</center>
+
+<h2><font size="6", color="black"><b><u>
+Provincias bañadas por el Mar Cantabrico:
+</font>
+</b></u></h2>
+
+<form>
+	<input/>
+	<input type="submit"/>
+</form>
+<table border="2">
+<tr>
+<th><font size="4"> Comunidades </font></th>
+<th><font size="4"> Provincias </font></th>
+</tr>
+<tr>
+<td><font color="red", size="4"> Galicia </font></td>
+<td><font color="red", size="4"> A Coruña </font></td>
+</tr>
+<tr>
+<td><font color="black", size="4"> Galicia </font></td>
+<td><font color="black", size="4"> Lugo </font></td>
+</tr>
+<tr>
+<td><font color="black", size="4"> Asturias </font></td>
+<td><font color="black", size="4"> Asturias </font></td>
+</tr>
+<tr>
+<td><font color="pink", size="4"> Cantabria </font></td>
+<td><font color="pink", size="4"> Cantabria </font></td>
+</font>
+</tr>
+</table>
+<p><b><font color="green", size="5">
+Provincias que son bañadas por el Cantabrico: Lugo, A Coruña, Asturias y Cantabria
+</font>
+</b></p>
+
+<h3>Modulos de ASIR</h3>
+<p> <font color="red"> ASIR1: PAR, ISO, GBD, FOL, LMS y FH </font></p>
+</br>
+<p><font color="blue"> ASIR2: SRI, ASGBD, ASO, EIE, IAW, SAD </font></p>
+</body>
+</html>
